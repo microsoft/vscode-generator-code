4b061ec3c87c9e99b8898d130ba9493365a41004
Martin Aeschlimann <martinae@microsoft.com>
2015-12-18 10:21:13 +0100

    add 'deactivate'

--- generators/app/templates/ext-command-js/extension.js
+++ generators/app/templates/ext-command-js/extension.js
@@ -23,3 +23,8 @@ function activate(context) {
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
--- generators/app/templates/ext-command-ts/src/extension.ts
+++ generators/app/templates/ext-command-ts/src/extension.ts
@@ -22,3 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	context.subscriptions.push(disposable);
 }
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
