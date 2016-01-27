8e02e7a93c26d94e8aa53d059274a12db9c8b1fa
Benjamin Pasero <benjpas@microsoft.com>
2016-01-27 15:06:51 +0100

    some polish

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,6 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
 'use strict';
 
 var yeoman = require('yeoman-generator');
