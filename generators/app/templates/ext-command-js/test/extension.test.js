20af6eee7d7b22f2236f6c2d1a7b7809a519392e
Martin Aeschlimann <martinae@microsoft.com>
2017-10-02 13:01:11 +0200

    Avoid var: use import, const or let (fixes #57)

--- generators/app/templates/ext-command-js/test/extension.test.js
+++ generators/app/templates/ext-command-js/test/extension.test.js
@@ -6,12 +6,12 @@
 //
 
 // The module 'assert' provides assertion methods from node
-var assert = require('assert');
+const assert = require('assert');
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-var vscode = require('vscode');
-var myExtension = require('../extension');
+const vscode = require('vscode');
+const myExtension = require('../extension');
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
