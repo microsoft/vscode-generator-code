c95f87d83c7963a15415dde78b0b25e469e6d72c
Erich Gamma <egamma@microsoft.com>
2017-11-25 08:15:36 +0100

    Fix the typings and extend the assert module

--- test/yeoman-assert.d.ts
+++ test/yeoman-assert.d.ts
@@ -1,5 +1,6 @@
-declare module "yeoman-assert" {
+
+export * from "assert";
+
+declare module "assert" {
     export function file(args: string | string[]): void;
-    export function deepEqual(actual: any, expected: any, message?: string): void;
-    export function equal(actual: any, expected: any, message?: string): void;
 }
