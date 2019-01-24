6a062e042b5d02ea17810cb8e9d7ecbe09f71ad0
Benjamin Pasero <benjpas@microsoft.com>
2019-01-24 10:29:17 +0100

    tweak testrunner comment

--- generators/app/templates/ext-command-js/test/index.js
+++ generators/app/templates/ext-command-js/test/index.js
@@ -12,8 +12,9 @@
 
 const testRunner = require('vscode/lib/testrunner');
 
-// You can directly control Mocha options by uncommenting the following lines
-// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+// You can directly control Mocha options by configuring the test runner below
+// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
+// for more info
 testRunner.configure({
 	ui: 'tdd', 		// the TDD UI is being used in extension.test.js (suite, test, etc.)
 	useColors: true // colored output from test results
