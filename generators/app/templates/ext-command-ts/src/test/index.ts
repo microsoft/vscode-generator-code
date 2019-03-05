6fd517ab1bc6997b240c05773a68010b0c131492
Benjamin Pasero <benjpas@microsoft.com>
2019-03-05 09:21:28 +0100

    fix #162

--- generators/app/templates/ext-command-ts/src/test/index.ts
+++ generators/app/templates/ext-command-ts/src/test/index.ts
@@ -5,8 +5,8 @@
 // By default the test runner in use is Mocha based.
 //
 // You can provide your own test runner if you want to override it by exporting
-// a function run(testRoot: string, clb: (error:Error) => void) that the extension
-// host can call to run the tests. The test runner is expected to use console.log
+// a function run(testsRoot: string, clb: (error: Error, failures?: number) => void): void
+// that the extension host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
