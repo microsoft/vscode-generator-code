aa3b2970ad0cb26488761c79086ff6a9443eafca
Daniel Imms <daimms@microsoft.com>
2019-05-28 15:43:55 -0700

    Fix indentation

--- generators/app/templates/ext-command-ts/src/extension.ts
+++ generators/app/templates/ext-command-ts/src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "<%= name %>" is now active!');
+	console.log('Congratulations, your extension "<%= name %>" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
