660080437a6e9647abc47b76218f46ac05d0f962
Pine Wu <octref@gmail.com>
2019-06-25 07:51:26 -0700

    Fix #176

--- generators/app/templates/ext-command-js/test/suite/index.js
+++ generators/app/templates/ext-command-js/test/suite/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Mocha = require('mocha');
 const glob = require('glob');
 
-function run(testsRoot, cb) {
+function run() {
 	// Create the mocha test
 	const mocha = new Mocha({
 		ui: 'tdd'
@@ -10,20 +10,28 @@ function run(testsRoot, cb) {
 	// Use any mocha API
 	mocha.useColors(true);
 
-	glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-		if (err) {
-			return cb(err);
-		}
+	return new Promise((c, e) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+			if (err) {
+				return e(err);
+			}
 
-		// Add files to the test suite
-		files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+			// Add files to the test suite
+			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-		try {
-			// Run the mocha test
-			mocha.run(failures => cb(null, failures));
-		} catch (err) {
-			cb(err);
-		}
+			try {
+				// Run the mocha test
+				mocha.run(failures => {
+					if (failures > 0) {
+						e(new Error(`${failures} tests failed.`));
+					} else {
+						c();
+					}
+				});
+			} catch (err) {
+				e(err);
+			}
+		});
 	});
 }
 
