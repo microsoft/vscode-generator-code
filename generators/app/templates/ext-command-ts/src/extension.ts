e9902445b29988a4b0678e33ab5a3ad760cb3e0a
Richard Willis <willis.rh@gmail.com>
2019-12-08 11:33:22 +0100

    Fix eslint errors

--- generators/app/templates/ext-command-ts/src/extension.ts
+++ generators/app/templates/ext-command-ts/src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
@@ -24,4 +24,5 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 export function deactivate() {}
