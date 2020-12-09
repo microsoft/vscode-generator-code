241f135c18485891419c2fcab5944bd388596cfd
Martin Aeschlimann <martinae@microsoft.com>
2020-12-09 19:49:47 +0100

    Remove `devtoolModuleFilenameTemplate` from webpack.config.js -- I don't think it is needed anymore Fixes #234

--- generators/app/templates/ext-command-ts/build/node-extension.webpack.config.js
+++ generators/app/templates/ext-command-ts/build/node-extension.webpack.config.js
@@ -14,8 +14,7 @@ const config = {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, '..', 'dist'),
     filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-    devtoolModuleFilenameTemplate: '../[resource-path]'
+    libraryTarget: 'commonjs2'
   },
   devtool: 'source-map',
   externals: {
