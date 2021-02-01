a3771ab77c36fdbe03394195fd426b689b1ddfa3
Martin Aeschlimann <martinae@microsoft.com>
2021-02-01 15:21:57 +0100

    update dependencies

--- generators/app/env.js
+++ generators/app/env.js
@@ -36,26 +36,25 @@ module.exports.getDependencyVersions = async function () {
         "@types/glob": "^7.1.3",
         "@types/mocha": "^8.0.4",
         "@types/node": "^12.11.7",
-        "@typescript-eslint/eslint-plugin": "^4.9.0",
-        "@typescript-eslint/parser": "^4.9.0",
-        "eslint": "^7.15.0",
+        "@typescript-eslint/eslint-plugin": "^4.14.1",
+        "@typescript-eslint/parser": "^4.14.1",
+        "eslint": "^7.19.0",
         "glob": "^7.1.6",
-        "mocha": "^8.1.3",
-        "typescript": "^4.1.2",
-        "vscode-test": "^1.4.1",
+        "mocha": "^8.2.1",
+        "typescript": "^4.1.3",
+        "vscode-test": "^1.5.0",
         "@types/webpack-env": "^1.16.0",
         "@types/vscode-notebook-renderer": "^1.48.0",
         "concurrently": "^5.3.0",
         "css-loader": "^4.2.0",
         "fork-ts-checker-webpack-plugin": "^5.0.14",
         "style-loader": "^1.2.1",
-        "ts-loader": "^8.0.11",
+        "ts-loader": "^8.0.14",
         "vscode-dts": "^0.3.1",
         "vscode-notebook-error-overlay": "^1.0.1",
-        "webpack": "^5.10.0",
-        "webpack-cli": "^4.2.0",
-        "webpack-dev-server": "^3.11.0"
-
+        "webpack": "^5.19.0",
+        "webpack-cli": "^4.4.0",
+        "webpack-dev-server": "^3.11.2"
     }
 }
 
