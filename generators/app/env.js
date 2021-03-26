cf30620d51581dd2a254b2409e6c4da866fa3a0a
Martin Aeschlimann <martinae@microsoft.com>
2021-03-26 15:18:25 +0000

    fetche vscode version only when necessary. Fixes #251

--- generators/app/env.js
+++ generators/app/env.js
@@ -2,37 +2,43 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 'use strict';
-var request = require('request-light');
+const request = require('request-light');
 
-var fallbackVersion = '^1.46.0';
-var promise = request.xhr({ url: 'https://update.code.visualstudio.com/api/releases/stable', headers: { "X-API-Version": "2" } }).then(res => {
-    if (res.status === 200) {
-        try {
-            var tagsAndCommits = JSON.parse(res.responseText);
-            if (Array.isArray(tagsAndCommits) && tagsAndCommits.length > 0) {
-                var segments = tagsAndCommits[0].version.split('.');
-                if (segments.length === 3) {
-                    return '^' + segments[0] + '.' + segments[1] + '.0';
+const fallbackVersion = '^1.46.0';
+let versionPromise = undefined;
+
+function getLatestVSCodeVersion() {
+    if (!versionPromise) {
+        versionPromise = request.xhr({ url: 'https://update.code.visualstudio.com/api/releases/stable', headers: { "X-API-Version": "2" } }).then(res => {
+            if (res.status === 200) {
+                try {
+                    var tagsAndCommits = JSON.parse(res.responseText);
+                    if (Array.isArray(tagsAndCommits) && tagsAndCommits.length > 0) {
+                        var segments = tagsAndCommits[0].version.split('.');
+                        if (segments.length === 3) {
+                            return '^' + segments[0] + '.' + segments[1] + '.0';
+                        }
+                    }
+                } catch (e) {
+                    console.log('Problem parsing version: ' + res.responseText, e);
                 }
+            } else {
+                console.warn('Unable to evaluate the latest vscode version: Status code: ' + res.status + ', ' + res.responseText);
             }
-        } catch (e) {
-            console.log('Problem parsing version: ' + res.responseText, e);
-        }
-    } else {
-        console.warn('Unable to evaluate the latest vscode version: Status code: ' + res.status + ', ' + res.responseText);
+            console.warn('Falling back to: ' + fallbackVersion);
+            return fallbackVersion;
+        }, err => {
+            console.warn('Unable to evaluate the latest vscode version: Error: ', err);
+            console.warn('Falling back to: ' + fallbackVersion);
+            return fallbackVersion;
+        });
     }
-    console.warn('Falling back to: ' + fallbackVersion);
-    return fallbackVersion;
-}, err => {
-    console.warn('Unable to evaluate the latest vscode version: Error: ', err);
-    console.warn('Falling back to: ' + fallbackVersion);
-    return fallbackVersion;
-});
-
-module.exports.getLatestVSCodeVersion = function () { return promise; };
+    return versionPromise;
+};
+module.exports.getLatestVSCodeVersion = getLatestVSCodeVersion;
 
 module.exports.getDependencyVersions = async function () {
-    const vscodeVersion = await promise;
+    const vscodeVersion = await getLatestVSCodeVersion();
     return {
         "@types/vscode": vscodeVersion,
         "@types/glob": "^7.1.3",
