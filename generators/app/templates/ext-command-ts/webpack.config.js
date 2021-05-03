7956bc30699ce6797923b3d041a58ea67bd0c01a
Martin Aeschlimann <martinae@microsoft.com>
2021-05-03 19:04:46 +0200

    comment about vsceignore in webpack.config

--- generators/app/templates/ext-command-ts/webpack.config.js
+++ generators/app/templates/ext-command-ts/webpack.config.js
@@ -19,6 +19,7 @@ const config = {
   devtool: 'nosources-source-map',
   externals: {
     vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vsceignore file
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
