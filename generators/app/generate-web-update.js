b49e5ec7b40fe591c36a1c8e3c600eebf249d333
tanhakabir <tanhakabir.ca@gmail.com>
2021-08-03 11:50:02 -0700

    fix typo for dependency

--- generators/app/generate-web-update.js
+++ generators/app/generate-web-update.js
@@ -50,7 +50,7 @@ module.exports = {
                 "package-web": "webpack --mode production --devtool hidden-source-map --config ./build/web-extension.webpack.config.js",
             },
             'devDependencies': {
-                'v@vscode/test-web': dependencyVersions['@vscode/test-web'],
+                '@vscode/test-web': dependencyVersions['@vscode/test-web'],
                 'ts-loader': dependencyVersions['ts-loader'],
                 'webpack': dependencyVersions['webpack'],
                 'webpack-cli': dependencyVersions['webpack-cli'],
