a494599555bb2ce25b36ff1ce903bfea2f8b6033
Martin Aeschlimann <martinae@microsoft.com>
2023-01-31 07:26:07 +0100

    running tests now requires 16.x (microsoft/vscode-test#181)

--- generators/app/templates/ext-command-js/test/runTest.js
+++ generators/app/templates/ext-command-js/test/runTest.js
@@ -15,7 +15,7 @@ async function main() {
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
-		console.error('Failed to run tests');
+		console.error('Failed to run tests', err);
 		process.exit(1);
 	}
 }
