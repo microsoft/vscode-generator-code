383f031b3414a12eb1c6ac89a5672d575bf1ce64
Martin Aeschlimann <martinae@microsoft.com>
2023-08-23 12:46:59 +0200

    prepare 1.7.8

--- generators/app/templates/ext-command-js/test/suite/index.js
+++ generators/app/templates/ext-command-js/test/suite/index.js
@@ -3,40 +3,43 @@ const Mocha = require('mocha');
 const glob = require('glob');
 
 function run() {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd',
+    color: true
+  });
 
-	const testsRoot = path.resolve(__dirname, '..');
+  const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+  return new Promise((c, e) => {
+    const testFiles = new glob.Glob('**/**.test.js', { cwd: testsRoot });
+    const testFileStream = testFiles.stream();
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+    testFileStream.on('data', (file) => {
+      // Add files to the test suite
+      mocha.addFile(path.resolve(testsRoot, file));
+    });
+    testFileStream.on('error', (err) => {
+      e(err);
+    });
+    testFileStream.on('end', () => {
+      try {
+        // Run the mocha test
+        mocha.run(failures => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        e(err);
+      }
+    });
+  });
 }
 
 module.exports = {
-	run
+  run
 };
