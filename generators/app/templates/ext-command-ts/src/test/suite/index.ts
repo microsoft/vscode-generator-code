c7233e2d2bb72671d1a530d4d7cdd687e0cc0fcf
Vincent Biret <vibiret@microsoft.com>
2023-09-28 08:18:32 -0400

    - fixes indent

--- generators/app/templates/ext-command-ts/src/test/suite/index.ts
+++ generators/app/templates/ext-command-ts/src/test/suite/index.ts
@@ -10,20 +10,20 @@ export async function run(): Promise<void> {
 	});
 
 	const testsRoot = path.resolve(__dirname, '..');
-    const files = await glob('**/**.test.js', { cwd: testsRoot });
+	const files = await glob('**/**.test.js', { cwd: testsRoot });
 
-    // Add files to the test suite
-    files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+	// Add files to the test suite
+	files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
 
-    try {
-        // Run the mocha test
-        mocha.run(failures => {
-            if (failures > 0) {
-                throw new Error(`${failures} tests failed.`);
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+	try {
+		// Run the mocha test
+		mocha.run(failures => {
+			if (failures > 0) {
+					throw new Error(`${failures} tests failed.`);
+			}
+		});
+	} catch (err) {
+		console.error(err);
+	}
 }
