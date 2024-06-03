533d8d306b97f675552c73f20acacbd1a2d6f3a9
Martin Aeschlimann <martinae@microsoft.com>
2024-06-03 16:15:54 +0200

    use const

--- generators/app/templates/ext-command-js/extension.js
+++ generators/app/templates/ext-command-js/extension.js
@@ -17,7 +17,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('<%= name %>.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('<%= name %>.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
